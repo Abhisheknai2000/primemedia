26d846dacd8e38ef7bd976d044a4b3e0ebdaa5e5
Abhisheknai2000 <115209579+Abhisheknai2000@users.noreply.github.com>
2022-10-07 22:13:24 +0530

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -28,7 +28,7 @@ async def filter(client, message):
                 return
         except UserNotParticipant:
             await client.send_message(
-                chat_id=channels.GetMessages,
+                chat_id=messages.GetChats,
                 text="**Please Join My Updates Channel to use this Bot!**",
                 reply_markup=InlineKeyboardMarkup(
                     [
